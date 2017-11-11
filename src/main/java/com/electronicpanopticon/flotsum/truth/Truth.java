6bf984e0290434a87e41916c3e73d9113d83ca40
Christoph <ignatz@gmail.com>
2017-11-11 12:24:31 -0500

    Added lombok, junit5 and log4j configured

---
+++ src/main/java/com/electronicpanopticon/flotsum/truth/Truth.java
@@ -0,0 +1,43 @@
+package com.electronicpanopticon.flotsum.truth;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EqualsAndHashCode
+public class Truth {
+
+    @Getter private final boolean isTrue;
+
+    public static Truth of(String v) {
+        switch (v.trim().toUpperCase()) {
+            case "T":
+            case "TRUE":
+            case "POSITIVE":
+            case "YES":
+            case "YUP":
+            case "Y":
+            case "AFFERMATIVE":
+            case "1":
+                return new Truth(true);
+            default:
+                if (v.trim().toUpperCase().substring(0, 2).equals("YA"))
+                    return new Truth(true);
+        }
+        return new Truth(false);
+    }
+
+    public static Truth of(boolean b) {
+        return new Truth(b);
+    }
+
+    public static Truth of(int i) {
+        return new Truth((i > 0));
+    }
+
+    @Override
+    public String toString() {
+        return isTrue ? "True" : "False";
+    }
+}
---
+++ src/test/java/com/electronicpanopticon/flotsum/truth/TruthTest.java
@@ -0,0 +1,49 @@
+package com.electronicpanopticon.flotsum.truth;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TruthTest {
+
+    public Truth subject;
+
+    @BeforeEach
+    public void before() {
+        subject = new Truth(true);
+    }
+
+    @Test
+    public void truth() {
+        assertTrue(subject.isTrue());
+        assertFalse(new Truth(false).isTrue());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "T", "true", "yeS", "1", "Yaaaaas", "Yaas", "affermative", "Yup" })
+    public void of_string(String argument) {
+        assertTrue(Truth.of(argument).isTrue());
+    }
+
+    @Test
+    public void of_int() {
+        assertTrue(Truth.of(1).isTrue());
+        assertTrue(Truth.of(3).isTrue());
+        assertFalse(Truth.of(0).isTrue());
+    }
+
+    @Test
+    public void toString_true_truth() {
+        assertEquals("True", Truth.of("yup").toString());
+    }
+
+    @Test
+    public void toString_false_true() {
+        assertEquals("False", Truth.of("nope").toString());
+    }
+}
